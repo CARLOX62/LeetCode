104e3be0b73e340e0a58289b35e20e4b4f41c04e
Aniket Kumar <148362225+CARLOX62@users.noreply.github.com>
2026-08-27 21:28:25 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.31%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def back(self,last,total,subset,k,n):
+        if total == n and len(subset) == k:
+            self.result.append(subset.copy())
+            return
+        for i in range(last,10):
+            sum = total + i
+            subset.append(i)
+            self.back(i+1,sum,subset,k,n)   
+            subset.pop() 
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        self.result = []
+        self.back(1,0,[],k,n)
+        return self.result
+
+        
