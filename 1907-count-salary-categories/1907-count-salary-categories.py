58169a048060bc4c4cd710d902fa48940f89ed2a
Aniket Kumar <148362225+CARLOX62@users.noreply.github.com>
2026-08-29 19:01:50 +0530

    Time: 356 ms (52.42%), Space: 89.9 MB (86.36%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+    low = ((accounts['income'] < 20000).sum())
+
+    Average = ((accounts['income'] >= 20000) &
+            (accounts['income'] <= 50000)).sum()
+
+    high = ((accounts['income'] > 50000)).sum()
+
+    result = pd.DataFrame({
+        'category':['Low Salary','Average Salary','High Salary'],
+        'accounts_count':[low,Average,high]
+    })
+
+    return result
