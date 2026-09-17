77495a368eb04981bee38f7210422a3a6378dac8
Aniket Kumar <148362225+CARLOX62@users.noreply.github.com>
2026-09-17 18:52:59 +0530

    Time: 1522 ms (16.33%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.salary >= (
+    SELECT MIN(salary)
+    FROM (
+        SELECT DISTINCT salary
+        FROM Employee
+        WHERE departmentId = e.departmentId
+        ORDER BY salary DESC
+        LIMIT 3
+    ) AS top_salaries
+);
