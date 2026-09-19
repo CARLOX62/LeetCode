6e0e631153e195e826b46e42d465d0556ea00aa1
Aniket Kumar <148362225+CARLOX62@users.noreply.github.com>
2026-09-19 20:26:28 +0530

    Time: 0 ms (100%), Space: 12.4 MB (22.55%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -1,20 +1,41 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def Inorder(self,root,result):
-        if root == None:
-            return 
-        self.Inorder(root.left,result)  
-        result.append(root.val)
-        self.Inorder(root.right,result)
-
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
         result = []
+        current = root
 
-        self.Inorder(root,result)
+        while current is not None:
+            # CASE 1: Current has no left child
+            if current.left is None:
+                result.append(current.val)
+                current = current.right
+            else:
+                # CASE 2: Current has left child
+                # Find the inorder predecessor
+                predecessor = current.left
 
-        return result
+                # Go to the rightmost node in left subtree
+                # Or find the node right child is current
+                while (predecessor.right is not None and predecessor.right != current):
+                    predecessor = predecessor.right
+
+                # Make current as right child of predecessor
+                if predecessor.right is None:
+                    predecessor.right = current
+                    current = current.left
+                else:
+                    # Revert the changes: remove the right child link
+                    predecessor.right = None
+                    result.append(current.val)
+                    current = current.right
+        return result                        
+        
